6a9ee9fcd90aa1fa2f2db08c4e87cc5fbf0d9b5e
Victor Lapa <victtorlapa1@gmail.com>
2022-03-17 15:48:21 -0300

    post e put de contacts

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -19,9 +19,56 @@ class ContractController {
     response.json(contact);
   }
 
-  store() {}
+  async store(request, response) {
+    const {
+      name, email, phone, category_id,
+    } = request.body;
 
-  update() {}
+    if (!name) {
+      return response.status(400).json({ error: 'field Name is required' });
+    }
+
+    const contactExists = await ContactsRepository.findByEmail(email);
+
+    if (contactExists) {
+      return response.status(400).json({ error: 'This email is already taken' });
+    }
+
+    const contact = await ContactsRepository.create({
+      name, email, phone, category_id,
+    });
+
+    response.json(contact);
+  }
+
+  async update(request, response) {
+    const { id } = request.params;
+
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    const contactExists = ContactsRepository.findById(id);
+    const emailExists = await ContactsRepository.findByEmail(email);
+
+    if (!contactExists) {
+      return response.status(404).json({ error: 'Contact not found' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'field Name is required' });
+    }
+
+    if (emailExists && emailExists.id !== id) {
+      return response.status(400).json({ error: 'This email is already taken' });
+    }
+
+    const contact = ContactsRepository.update(id, {
+      name, email, phone, category_id,
+    });
+
+    response.json(contact);
+  }
 
   async delete(request, response) {
     const { id } = request.params;
